9624c37be1e99126cdfe1a17870f63c905649b40
725216 <djp.1@qq.com>
2021-08-28 09:31:08 +0000

    Update ssh-server.sh

--- ssh-server.sh
+++ ssh-server.sh
@@ -9,7 +9,7 @@ if [[ -z "$SSH_PASSWORD" ]]; then
   echo "Please set 'SSH_PASSWORD' for user: $USER"
   exit 3
 fi
-
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync libncurses5-dev gawk libcloog-isl-dev ncftp
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
